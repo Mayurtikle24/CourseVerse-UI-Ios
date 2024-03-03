e19ae2f324503614dbf420ee23ff96285c943f18
Mayur Tikle <rishi.bijlani@gmail.com>
2024-03-03 18:28:20 +0530

    Initial Commit

---
+++ CourseUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CourseUI
+//
+//  Created by CW Mayur Tikle on 03/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CourseUI/CourseUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseUIApp.swift
+//  CourseUI
+//
+//  Created by CW Mayur Tikle on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CourseUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
